753bb657b094f86b23ba5502b94b74b05a39642e
Henrikas <gyngga@gmail.com>
2018-01-26 21:47:27 +0200

    Main menu

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,11 @@
+﻿public static class Constants
+{
+    public enum SceneIndex
+    {
+        MainMenu = 0,
+        Game1 = 1,
+        HowToPlay = 2,
+        Credits = 3,
+        Options = 4
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene((int)Constants.SceneIndex.Game1);
+    }
+
+    public void LoadHowTo()
+    {
+        SceneManager.LoadScene((int)Constants.SceneIndex.HowToPlay);
+    }
+
+    public void LoadOptions()
+    {
+        SceneManager.LoadScene((int)Constants.SceneIndex.Options);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene((int)Constants.SceneIndex.Credits);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
