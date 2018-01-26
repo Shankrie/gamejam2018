248414225d0f542031b85760bc2d96ab2b73dce1
Henrikas <gyngga@gmail.com>
2018-01-26 22:43:09 +0200

    In-level menu

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour {
+
+    public GameObject GameMenuDialog;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameMenuDialog.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene((int)Constants.SceneIndex.MainMenu);
+    }
+
+    public void ResumeGame()
+    {
+        GameMenuDialog.SetActive(false);
+        Time.timeScale = 1;
+    }
+}
